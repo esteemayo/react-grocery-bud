26caa6d0fd57a1803c4911ee86c13cfa88c998c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-23 01:51:32 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -73,8 +73,8 @@ function App() {
   };
 
   return (
-    <section className="section-center">
-      <form onSubmit={handleSubmit} className="grocery-form">
+    <section className='section-center'>
+      <form onSubmit={handleSubmit} className='grocery-form'>
         {alert.show && <Alert {...alert} hideAlert={showAlert} list={list} />}
         <h3>Grocery Bud</h3>
         <div className="form-control">
