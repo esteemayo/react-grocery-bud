32610d8390ba7ce8189c2e83bc939a195a55cac2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-23 01:51:08 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
   };
 
   const handleClearList = () => {
-    showAlert("true", "danger", "Empty list");
+    showAlert('true', 'danger', 'Empty list');
     setList([]);
   };
 
