bb2da766746054ff5c599821cda788fe1013f1e7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-23 01:52:54 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import List from './components/List';
+import List from 'components/List';
 import Alert from './components/Alert';
 import Button from './components/Button';
 
